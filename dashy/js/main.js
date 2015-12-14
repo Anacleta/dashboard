4e31ea9f5fb8413e9fd58888a1f9503b41cf03ab
Anacleta <mercypalacios502@gmail.com>
2015-12-14 11:25:24 -0500

    commit

---
+++ dashy/js/main.js
@@ -0,0 +1,13 @@
+var a = 1 ;
+var b = 2;
+var texto = 'hola soy un texto'
+
+/*para ver si es falso*/
+function maximoDeDosNumeros(numero1,numero2)
+{
+	if (numero1>numero2) {
+		return numero1;
+	}else{
+		return numero2;
+	}
+}
